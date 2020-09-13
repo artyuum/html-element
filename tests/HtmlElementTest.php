ff64530f2e146407b7f38f1fad19d5b9b8676a20
Artyum <artyum@protonmail.com>
2020-09-13 23:10:08 +0400

    Set the proper namespace for the HtmlElementTest class

--- tests/HtmlElementTest.php
+++ tests/HtmlElementTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace Artyum\HtmlElement\Tests;
 
 use Artyum\HtmlElement\Exceptions\SelfClosingTagException;
 use Artyum\HtmlElement\Exceptions\WrongAttributeValueException;
